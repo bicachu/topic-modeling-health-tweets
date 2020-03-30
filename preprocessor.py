b7ff6b3317c1fe344ac9cb9a0ae4895510de8b02
bicachu <bicajohn1@gmail.com>
2020-03-29 23:44:39 -0400

    added code to remove dupes

--- preprocessor.py
+++ preprocessor.py
@@ -18,6 +18,9 @@ tweets_df = tweets_df.dropna(axis='columns')
 tweets_df = tweets_df.drop(columns=['conversation_id', 'created_at', 'cashtags', 'user_id',
                                     'user_id_str', 'link', 'search', 'reply_to'])
 
+# Remove any duplicates based on exact tweet matches
+tweets_df.drop_duplicates(keep=False, inplace=True, subset="tweet")
+
 # Export preprocessed clean data set to csv
 tweets_df.to_csv(r'health_tweets.csv', index=False, header=True)
 
