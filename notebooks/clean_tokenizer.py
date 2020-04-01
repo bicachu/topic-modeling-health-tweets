47fed74f701cc0c8a18775116f131d777a018a1d
bicachu <bicajohn1@gmail.com>
2020-04-01 11:38:35 -0400

    added clean tokenizer script

---
+++ notebooks/clean_tokenizer.py
@@ -0,0 +1,88 @@
+"""
+@author: John Bica | Christopher Tso
+Script to clean tweet data and tokenize
+"""
+import pandas as pd
+import numpy as np
+import re
+import nltk
+import gensim
+import tqdm
+
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from tqdm.notebook import trange, tqdm
+
+
+# Download nltk word libraries if not present
+# nltk.download('wordnet')
+# nltk.download('stopwords')
+
+punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@'         # define a string of punctuation symbols
+
+# Functions to clean tweets
+def remove_links(tweet):
+    """Takes a string and removes web links from it"""
+    tweet = re.sub(r'http\S+', '', tweet)   # remove http links
+    tweet = re.sub(r'bit.ly/\S+', '', tweet) # remove bitly links
+    tweet = tweet.strip('[link]')   # remove [links]
+    tweet = re.sub(r'pic.twitter\S+','', tweet)
+    return tweet
+
+def remove_users(tweet):
+    """Takes a string and removes retween and @user information"""
+    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet) # remove retweet
+    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet) # remove tweeted at
+    return tweet
+
+
+def lemmatize(tweet):
+    """Returns tokenized representation of words in lemma form excluding stopwords"""
+    result = []
+    for token in gensim.utils.simple_preprocess(tweet):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(
+                token) > 3:  # drops words with 3 or less characters
+            result.append(lemmatize_stemming(token))
+    return result
+
+
+def lemmatize_stemming(token):
+    """Returns lemmatization of a token"""
+    return WordNetLemmatizer().lemmatize(token, pos='v')
+
+
+def clean_tweet(tweet, bigrams=False):
+    """Main master function to clean tweets, stripping noisy characters and tokenizing use lemmatization"""
+    tweet = remove_users(tweet)
+    tweet = remove_links(tweet)
+    tweet = tweet.lower()  # lower case
+    tweet = re.sub('[' + punctuation + ']+', ' ', tweet)  # strip punctuation
+    tweet = re.sub('\s+', ' ', tweet)  # remove double spacing
+    tweet = re.sub('([0-9]+)', '', tweet)  # remove numbers
+
+    tweet_token_list = lemmatize(tweet)  # apply lemmatization and tokenization
+
+    if bigrams:
+        tweet_token_list = tweet_token_list + [tweet_token_list[i] + '_' + tweet_token_list[i + 1]
+                                               for i in range(len(tweet_token_list) - 1)]
+    tweet = ' '.join(tweet_token_list)
+    return tweet
+
+
+def tokenize_tweets(data_path):
+    """Main function to read in and return clean data set.
+    This can be used in Jupyter notebooks by importing this module and calling the main() function
+
+    Args:
+        data_path = path to input data set .csv file
+
+    Returns:
+        pandas data frame with cleaned tokens
+    """
+
+    tweets_df = pd.read_csv(data_path)
+    tweets_df['clean_tweets'] = tweets_df.tweet.apply(clean_tweet)
+    num_tweets = len(tweets_df)
+    print('Complete. Number of Tweets that have been cleaned and tokenized : {}'.format(num_tweets))
+    return tweets_df
